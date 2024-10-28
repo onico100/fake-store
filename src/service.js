3d495b95009ca64813c4072f5de9a59f57cc6000
onico100 <142163577+onico100@users.noreply.github.com>
2024-10-28 14:05:38 +0200

    add books api form node server

--- src/service.js
+++ src/service.js
@@ -1,3 +1,4 @@
 export function getItems(itemsName) {
+  if (itemsName === "books") return fetch("http://localhost:5000/books");
   return fetch(`https://fakestoreapi.com/products/category/${itemsName}`);
 }
