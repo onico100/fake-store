28ea8de11cdde2c64572ba77cac2cc9bff335487
onico100 <142163577+onico100@users.noreply.github.com>
2024-10-28 10:36:42 +0200

    call fake data with props

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route
                   path="/Electronicts"
-                  element={<ItemConteiner itemsName={"Electronicts"} />}
+                  element={<ItemConteiner itemsName={"Electronics"} />}
                 />
                 <Route
                   path="/Jewlery"
--- src/service.js
+++ src/service.js
@@ -1,7 +1,3 @@
-export function getElectronics() {
-  return fetch("https://fakestoreapi.com/products/category/electronics");
-}
-
-export function getJewelery() {
-  return fetch("https://fakestoreapi.com/products/category/jewelery");
+export function getItems(itemsName) {
+  return fetch(`https://fakestoreapi.com/products/category/${itemsName}`);
 }
